ddedafbaefb05f0210e7430bdcee504477bc8abc
Đậu Đức Quân <bruceparkerblue@gmail.com>
2025-01-04 17:53:42 +0700

    Adding helper files

---
+++ app/src/main/java/com/kitepea/currency_converter/helper/EndPoints.kt
@@ -0,0 +1,12 @@
+package com.kitepea.currency_converter.helper
+
+class EndPoints {
+    companion object {
+        const val BASE_URL = "https://api.getgeoapi.com/api/v2/currency/"
+        const val API_KEY = "5b5c727655a592dc810a517132e71cc3532b2b3b"
+
+        // Convert URL
+        const val CONVERT_URL = "convert"
+
+    }
+}
---
+++ app/src/main/java/com/kitepea/currency_converter/helper/Utility.kt
@@ -0,0 +1,53 @@
+package com.kitepea.currency_converter.helper
+
+import android.app.Activity
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+
+fun hideKeyboard(activity: Activity) {
+    val inputMethodManager: InputMethodManager =
+        activity.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+
+    //Find the currently focused view
+    var view: View? = activity.currentFocus
+    if (view == null) {
+        view = View(activity)
+    }
+    inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+}
+
+fun isNetworkAvailable(context: Context?): Boolean {
+    if (context == null) return false
+    val connectivityManager =
+        context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+        val capabilities =
+            connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+        if (capabilities != null) {
+            when {
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> {
+                    return true
+                }
+
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> {
+                    return true
+                }
+
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> {
+                    return true
+                }
+            }
+        }
+    } else {
+        val activeNetwork = connectivityManager.activeNetwork
+        val networkCapabilities = connectivityManager.getNetworkCapabilities(activeNetwork)
+        if (networkCapabilities != null && networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)) {
+            return true
+        }
+    }
+    return false
+}
